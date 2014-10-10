ae02a7d89755c0a478868a8ad6f2c4134debb74d
Eric Wendelin <ewendelin@twitter.com>
2014-10-09 20:51:50 -0600

    Use Sauce Launcher to launch various browsers for testing.

--- karma.conf.js
+++ karma.conf.js
@@ -1,66 +1,67 @@
-// Karma configuration
-// Generated on Thu Oct 09 2014 18:14:44 GMT-0600 (MDT)
+module.exports = function (config) {
+    // Check out https://saucelabs.com/platforms for all browser/platform combos
+    var customLaunchers = {
+        sl_chrome: {
+            base: 'SauceLabs',
+            browserName: 'chrome',
+            platform: 'Windows 7',
+            version: '37'
+        },
+        sl_firefox: {
+            base: 'SauceLabs',
+            browserName: 'firefox',
+            version: '32'
+        },
+        sl_safari: {
+            base: 'SauceLabs',
+            browser: 'safari',
+            os: 'OS X 10.8',
+            'browser-version': '6'
+        },
+        sl_ios_safari: {
+            base: 'SauceLabs',
+            browserName: 'iphone',
+            platform: 'OS X 10.9',
+            version: '7.1'
+        },
+        sl_ie_10: {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            platform: 'Windows 7',
+            version: '10'
+        },
+        sl_ie_11: {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            platform: 'Windows 8.1',
+            version: '11'
+        },
+        sl_opera_12: {
+            browserName: 'opera',
+            platform: 'Windows 7',
+            version: '12'
+        }
+    };
 
-module.exports = function(config) {
-  config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      'stackframe.js',
-      'spec/*-spec.js'
-    ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome', 'Opera', 'Safari', 'IE', 'PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  });
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            'stackframe.js',
+            'spec/*-spec.js'
+        ],
+        exclude: [],
+        preprocessors: {},
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        sauceLabs: {
+            testName: 'stackframe unit tests'
+        },
+        customLaunchers: customLaunchers,
+        browsers: Object.keys(customLaunchers),
+        reporters: ['dots', 'saucelabs'],
+        singleRun: true
+    });
 };
