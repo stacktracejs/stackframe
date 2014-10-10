77ad32c4e929fa00727a194ad56dcc1d2e1d648e
Eric Wendelin <ewendelin@twitter.com>
2014-10-09 20:26:16 -0600

    Test recent major browsers with Travis CI and Sauce Labs.

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'ChromeCanary', 'Opera', 'Safari', 'PhantomJS'],
+    browsers: ['Firefox', 'Chrome', 'Opera', 'Safari', 'IE', 'PhantomJS'],
 
 
     // Continuous Integration mode
