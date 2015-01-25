cd4f5bb22507b41120e2e47895fabf9780f34afb
Eric Wendelin <ewendelin@twitter.com>
2015-01-25 13:14:36 -0700

    Add a bunch of additional browsers to CI.

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -4,28 +4,93 @@ module.exports = function (config) {
         process.exit(1);
     }
 
+    // Commented some of these out just so CI doesn't take forever.
     // Check out https://saucelabs.com/platforms for all browser/platform combos
     var customLaunchers = {
-        sl_chrome: {
+        //slIOS7: {
+        //    base: 'SauceLabs',
+        //    browserName: 'iPhone',
+        //    platform: 'OS X 10.9',
+        //    version: '7.1'
+        //},
+        slIOS8: {
+            base: 'SauceLabs',
+            browserName: 'iPhone',
+            platform: 'OS X 10.9',
+            version: '8.1'
+        },
+        //slAndroid4: {
+        //    base: 'SauceLabs',
+        //    browserName: 'Android',
+        //    platform: 'Linux',
+        //    version: '4.0'
+        //},
+        slChrome: {
             base: 'SauceLabs',
             browserName: 'chrome'
         },
-        sl_firefox: {
+        slFirefox: {
             base: 'SauceLabs',
             browserName: 'firefox'
         },
-        sl_ie_11: {
+        slSafari6: {
+            base: 'SauceLabs',
+            browserName: 'safari',
+            platform: 'OS X 10.8',
+            version: '6'
+        },
+        //slSafari7: {
+        //    base: 'SauceLabs',
+        //    browserName: 'safari',
+        //    platform: 'OS X 10.9',
+        //    version: '7'
+        //},
+        //slSafari8: {
+        //    base: 'SauceLabs',
+        //    browserName: 'safari',
+        //    platform: 'OS X 10.10',
+        //    version: '8'
+        //}
+        slOpera: {
+            base: 'SauceLabs',
+            browserName: 'opera'
+        },
+        slIE11: {
             base: 'SauceLabs',
             browserName: 'internet explorer',
             platform: 'Windows 8.1',
             version: '11'
         },
-        sl_ie_9: {
+        //slIE10: {
+        //    base: 'SauceLabs',
+        //    browserName: 'internet explorer',
+        //    platform: 'Windows 8',
+        //    version: '10'
+        //},
+        //slIE9: {
+        //    base: 'SauceLabs',
+        //    browserName: 'internet explorer',
+        //    platform: 'Windows 7',
+        //    version: '9'
+        //}
+        //slIE8: {
+        //    base: 'SauceLabs',
+        //    browserName: 'internet explorer',
+        //    platform: 'Windows XP',
+        //    version: '8'
+        //}
+        slIE7: {
             base: 'SauceLabs',
             browserName: 'internet explorer',
-            platform: 'Windows 7',
-            version: '9'
+            platform: 'Windows XP',
+            version: '7'
         }
+        //slIE6: {
+        //    base: 'SauceLabs',
+        //    browserName: 'internet explorer',
+        //    platform: 'Windows XP',
+        //    version: '6'
+        //}
     };
 
     config.set({
