ee8d8ba2a013c613827e21f7eaff3b4e5f02c341
Eric Wendelin <me@eriwen.com>
2018-10-23 17:31:56 -0700

    Migrate to ESLint for linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var coveralls = require('gulp-coveralls');
 var del = require('del');
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 var karma = require('karma');
 var rename = require('gulp-rename');
 var runSequence = require('run-sequence');
@@ -9,6 +10,13 @@ var uglify = require('gulp-uglify');
 
 var sources = 'stackframe.js';
 var minified = sources.replace('.js', '.min.js');
+
+gulp.task('lint', function() {
+    return gulp.src(sources)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
 
 gulp.task('test', function(done) {
     new karma.Server({
